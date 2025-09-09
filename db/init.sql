0b51060fcf95f377b4aca6c09a8b14c61eb9c310
Unkn0wncod3 <97988163+Unkn0wncod3@users.noreply.github.com>
2025-09-09 14:10:25 +0200

    New init sql table

--- db/init.sql
+++ db/init.sql
@@ -4,4 +4,16 @@ CREATE TABLE IF NOT EXISTS notes (
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE IF NOT EXISTS files (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    person_name TEXT NOT NULL,
+    date_of_birth DATE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+
 INSERT INTO notes (title) VALUES ('Hello from Postgres + Docker!');
+
+INSERT INTO files (title, person_name, date_of_birth)
+VALUES ('Erste Akte', 'Max Mustermann', '1990-05-15');
